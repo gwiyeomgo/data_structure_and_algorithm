488044242794dcf9909a9be12477af7b25017574
gwiyeomgo <gwiyeomgo@gmail.com>
2023-12-25 11:32:40 +0900

    update docs-20231225

--- publish_gitbook.sh
+++ publish_gitbook.sh
@@ -14,7 +14,7 @@ cp -R _book/* docs/
 git clean -fx _book
 # upload
  git add .
- git commit -a -m "update docs-20230816"
+ git commit -a -m "update docs-20231225"
  git push -u origin master
 
 ## gitbook serve
